f8b0822e42c4b05c1d09def54be897bf8ec28352
Jagdeep Gill <jgill.tftf@gmail.com>
2016-03-26 23:53:58 -0700

    [mqtt,refactor] refactored MQTTPublisher to extract a BrokerConnection class, network operations are now done using worker threads instead of the app's main thread

---
+++ app/src/main/java/mqtt/BrokerConnection.java
@@ -0,0 +1,290 @@
+package mqtt;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.eclipse.paho.client.mqttv3.IMqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * This class is used to manage a connection to an MQTT broker. The network operations are
+ * performed asyncronously (on worker threads).
+ *
+ * Typical usage:
+ * 1. Construct a BrokerConnection
+ * 2. Call startAndWaitForConnectionToBroker()
+ * 3. call getMqttClient to obtain a configured MQTT mqttClient
+ * 4. To stop the connection, call stopConnectionToBroker()
+ *
+ */
+public class BrokerConnection {
+
+    private static final String TAG = "SomeApp";
+
+    private String BROKER_URL;
+    private String USERNAME = "defaultwatch";
+    private String PASSWORD = "vandricowatch";
+
+    private String urlServingBrokerIP =
+            "https://storage.googleapis.com/ssl-team10-cs319/broker_ips.txt"; // file containing broker's IP address
+    private String brokerInsecurePort = "1883";
+    private String brokerSecurePort = "8883";
+
+    private MqttPublisher publisher;
+    private MqttConnectOptions connectOptions;
+    private IMqttAsyncClient mqttClient;
+    private boolean ENCRYPT;
+
+    private Context parentContext;
+    private String clientId;
+
+    private boolean isConnectedToBroker;
+
+
+    public BrokerConnection(Context parentContext, String clientId, MqttPublisher publisher, boolean encrypted){
+        this.parentContext = parentContext;
+        this.clientId = clientId;
+        this.publisher = publisher;
+        this.ENCRYPT = encrypted;
+        this.connectOptions = createConnectOptions();
+        this.isConnectedToBroker = false;
+    }
+
+    public void startAndWaitForConnectionToBroker() throws ConnectivityException {
+
+        setBrokerUrl(); // must be done before creating a client
+        setMqttClient(createMqttClient());
+
+        ConnectToBrokerTask task = new ConnectToBrokerTask();
+        task.execute();
+        try {
+            ConnectivityException e = (ConnectivityException) task.get(); // wait until connected
+            if (e != null){
+                throw e;
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void stopConnection() throws ConnectivityException {
+        try {
+            getMqttClient().disconnect();
+        } catch (MqttException e) {
+            throw new ConnectivityException(e.getMessage());
+        }
+    }
+
+    private MqttConnectOptions createConnectOptions() {
+
+        MqttConnectOptions connectOptions = new MqttConnectOptions();
+
+        System.out.println("# Starting connect optinos");
+        connectOptions.setUserName(USERNAME);
+        connectOptions.setPassword(PASSWORD.toCharArray());
+
+        if (ENCRYPT){
+            SSLSupplier sslSupplier = new HttpSSLSupplier();
+            try {
+                sslSupplier.setupSSL();
+            } catch (CertificateException | NoSuchAlgorithmException | KeyStoreException | IOException | KeyManagementException e) {
+                e.printStackTrace();
+            }
+
+            connectOptions.setSocketFactory(sslSupplier.getSslSocketFactory());
+        }
+
+        System.out.println("# Reached end of connection options");
+        return connectOptions;
+    }
+
+    private IMqttAsyncClient createMqttClient(){
+        MqttAsyncClient mqttClient = null;
+
+        if (getBrokerUrl() != null){
+            try {
+                // We can use file or memory persistence, for now use memory since it's a bit simpler
+                // must set the app's writable directory, default is root dir. which is not writable!
+                //String appCacheDir = getParentContext().getCacheDir().getAbsolutePath();
+                //MqttDefaultFilePersistence fp = new MqttDefaultFilePersistence(appCacheDir);
+                MemoryPersistence mp = new MemoryPersistence();
+                mqttClient = new MqttAsyncClient(BROKER_URL, clientId, mp);
+            } catch (MqttException e) {
+                System.out.println("Problem setting the MqttAsyncClient");
+            }
+        } else {
+            throw new Error("Broker url and/or port are null");
+        }
+        System.out.println("### Created mqtt client");
+        return mqttClient;
+    }
+
+    public boolean isConnectedToInternet(){
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getParentContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null) {
+            return networkInfo.isConnected();
+        } else {
+            throw new Error("Null NetworkInfo");
+        }
+    }
+
+    private String getBrokerUrl() {
+        return BROKER_URL;
+    }
+
+    private void setBrokerUrl() throws ConnectivityException {
+//        BROKER_URL = "ssl://54.92.237.174:27981"; // mqtt cloud
+
+        SetBrokerUrlTask task = new SetBrokerUrlTask();
+        task.execute();
+        try {
+            ConnectivityException e = (ConnectivityException) task.get();
+            if (e != null){
+                throw e;
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private MqttConnectOptions getConnectOptions() {
+        return connectOptions;
+    }
+
+    public IMqttAsyncClient getMqttClient() {
+        return mqttClient;
+    }
+
+    private void setMqttClient(IMqttAsyncClient mqttAsyncClient) {
+        this.mqttClient = mqttAsyncClient;
+    }
+
+    public Context getParentContext() {
+        return parentContext;
+    }
+
+    public boolean getIsConnectedToBroker(){
+        return this.isConnectedToBroker;
+    }
+
+    public boolean setIsConnectedToBroker(boolean status){
+        this.isConnectedToBroker = status;
+        return status;
+    }
+
+    private class ConnectToBrokerTask extends AsyncTask {
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            System.out.println("#### Starting async task: connect to broker");
+            ConnectivityException exception = null;
+            try {
+                startConnectionToBroker();
+            } catch (ConnectivityException e) {
+                exception = e;
+            }
+            System.out.println("#### Finished async task: connected to broker? " + getIsConnectedToBroker());
+            return exception;
+        }
+
+        private void startConnectionToBroker() throws ConnectivityException {
+            if (!isConnectedToInternet()) {
+                throw new ConnectivityException("Network unavailable");
+            }
+
+            IMqttToken connectToken = null;
+            try {
+                connectToken = getMqttClient().connect(getConnectOptions());
+                connectToken.waitForCompletion();
+                setIsConnectedToBroker(true);
+            } catch (MqttException e) {
+                String msg = e.getMessage();
+                if (connectToken != null) {
+                    msg += "\t" + connectToken.getResponse();
+                }
+                throw new ConnectivityException(msg);
+            }
+
+            getMqttClient().setCallback(publisher);
+        }
+    }
+
+    private class SetBrokerUrlTask extends AsyncTask{
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            ConnectivityException exception = null;
+            System.out.println("#### Starting async task: set broker url");
+            try {
+                findBrokerUrl();
+            } catch (IOException e) {
+                exception = new ConnectivityException(e);
+            }
+            System.out.println("#### Finished async task: set broker url to " + getBrokerUrl());
+            return exception;
+        }
+
+        private void findBrokerUrl() throws IOException {
+            // Adapted from http://developer.android.com/training/basics/network-ops/connecting.html
+            InputStream is = null;
+            int len = 500;
+
+
+            URL url = new URL(urlServingBrokerIP);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d(TAG, "Http response when getting broker IP address: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            String ipAddress = reader.readLine();
+
+            System.out.println("IP data: " + ipAddress);
+
+            if (ENCRYPT) {
+                BROKER_URL = "tcp://" + ipAddress + ":" + brokerSecurePort;
+            } else {
+                BROKER_URL = "tcp://" + ipAddress + ":" + brokerInsecurePort;
+            }
+
+            System.out.println("Broker found at: " + BROKER_URL);
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it
+            is.close();
+
+        }
+    }
+}
--- app/src/main/java/mqtt/MqttPublisher.java
+++ app/src/main/java/mqtt/MqttPublisher.java
@@ -1,18 +1,17 @@
 package mqtt;
 
 import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.widget.TextView;
 
+import org.eclipse.paho.client.mqttv3.IMqttAsyncClient;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 import java.nio.charset.StandardCharsets;
 import java.security.KeyManagementException;
@@ -30,43 +29,42 @@ import javax.net.ssl.SSLContext;
  * Starting and closing the connection are handled at the time of publishing data
  *
  */
-// TODO: app freezes if wifi is lost after it starts
-public class MqttPublisher implements MqttCallback {
-    static final String TAG = "SomeApp";
-    // Our own brokers:
-    // unencrypted: tcp://130.211.153.252:1883
-    // encrypted: tcp://130.211.153.252:8883
-    //private static final String BROKER_URL = "tcp://130.211.153.252:1883";  // google cloud
-    //private static final String USERNAME = "ehcxlgcl";
-    //private static final String PASSWORD = "AQsUmTw6wYee";
-    private static final String BROKER_URL = "ssl://54.92.237.174:27981"; // mqtt cloud
-    private static final String USERNAME = "ehcxlgcl";
-    private static final String PASSWORD = "AQsUmTw6wYee";
 
-    private ConnectivityManager connMgr;
+public class MqttPublisher implements MqttCallback {
+
+    static final String TAG = "SomeApp";
+
+    public boolean ENCRYPT = false;
+    private BrokerConnection brokerConnection;
+    private IMqttAsyncClient mqttClient;
+
     private String clientId;
-    private MqttAsyncClient client;
-    private MqttConnectOptions connectOptions;
+
+    private Context parentContext;
 
     public MqttPublisher(String clientId, Context parentContext){
         this.clientId = clientId;
-        connMgr = (ConnectivityManager) parentContext.getSystemService(Context.CONNECTIVITY_SERVICE);
-        setConnectOptions();
-        setupClient();
+        this.parentContext = parentContext;
+        this.brokerConnection = new BrokerConnection(getParentContext(), getClientId(), this, getIsEncrypted());
+    }
+
+    public void startConnection() throws ConnectivityException {
+        brokerConnection.startAndWaitForConnectionToBroker();
+        this.mqttClient = brokerConnection.getMqttClient();
+    }
+
+    public void stopConnection() throws ConnectivityException {
+        brokerConnection.stopConnection();
     }
 
     // startConnection must be called before publish
     public void publish(TopicMsg tm) throws ConnectivityException {
-        // TODO: app freezes at startup if no wifi
-        // make sure we're connected to the internet
-        if (networkAvailable()) {
-            // if connected: start connection, publish to broker, then close connection
-            // startConnection();
+        if (connectedToBroker()) {
             String topic = tm.getTopic();
             String _msg = tm.getMsg();
             MqttMessage msg = new MqttMessage(_msg.getBytes());
             try {
-                IMqttToken sendToken = client.publish(topic, msg);
+                IMqttToken sendToken = mqttClient.publish(topic, msg);
                 sendToken.waitForCompletion();
                 Log.i(TAG, "message sent");
             } catch (MqttException e) {
@@ -74,33 +72,6 @@ public class MqttPublisher implements MqttCallback {
             }
         } else {
             throw new ConnectivityException("Network unavailable");
-        }
-    }
-
-    public void startConnection() throws ConnectivityException {
-        if (! networkAvailable())
-            throw new ConnectivityException("Network unavailable");
-
-        IMqttToken connectToken = null;
-        try {
-            connectToken = client.connect(connectOptions);
-            connectToken.waitForCompletion();
-        } catch (MqttException e) {
-            String msg = e.getMessage();
-            if (connectToken != null){
-                msg += "\t" + connectToken.getResponse();
-            }
-            throw new ConnectivityException(msg);
-        }
-
-        client.setCallback(this);
-    }
-
-    public void stopConnection() throws ConnectivityException {
-        try {
-            client.disconnect();
-        } catch (MqttException e) {
-            throw new ConnectivityException(e.getMessage());
         }
     }
 
@@ -133,51 +104,29 @@ public class MqttPublisher implements MqttCallback {
         */
     }
 
-    public MqttAsyncClient getClient() {
-        return client;
+    public boolean getIsEncrypted(){
+        return ENCRYPT;
     }
 
-    public void setClient(MqttAsyncClient client) {
-        this.client = client;
+    public boolean setIsEncrypted(){
+        return ENCRYPT;
     }
 
-    public void setupClient(){
-        if (getBrokerUrl() != null){
-            try {
-                // We can use file or memory persistence, for now use memory since it's a bit simpler
-                // must set the app's writable directory, default is root dir. which is not writable!
-                //String appCacheDir = getParentContext().getCacheDir().getAbsolutePath();
-                //MqttDefaultFilePersistence fp = new MqttDefaultFilePersistence(appCacheDir);
-                MemoryPersistence mp = new MemoryPersistence();
-                setClient(new MqttAsyncClient(BROKER_URL, clientId, mp));
-            } catch (MqttException e) {
-                System.out.println("Problem setting the MqttAsyncClient");
-            }
-        } else {
-            throw new Error("Broker url and/or port are null");
-        }
+    public String getClientId() {
+        return clientId;
     }
 
-    public String getBrokerUrl() {
-        return BROKER_URL;
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
     }
 
-    public void setConnectOptions() {
-        connectOptions = new MqttConnectOptions();
-        connectOptions.setUserName(USERNAME);
-        connectOptions.setPassword(PASSWORD.toCharArray());
-        try {
-            SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
-            sslContext.init(null, null, null);
-            connectOptions.setSocketFactory(sslContext.getSocketFactory());
-        } catch (NoSuchAlgorithmException | KeyManagementException e) {
-            e.printStackTrace();
-        }
+    public Context getParentContext() {
+        return parentContext;
     }
 
-    private boolean networkAvailable() {
-        NetworkInfo info = connMgr.getActiveNetworkInfo();
-        return info != null && info.isConnected();
+    private boolean connectedToBroker(){
+        return brokerConnection.isConnectedToInternet() && brokerConnection.getIsConnectedToBroker();
     }
+
 }
 
